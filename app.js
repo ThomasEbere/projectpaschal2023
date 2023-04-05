ccb0daf32a5c1c495b81c15f8661c5f6aac20c4d
Thommyebere <75401236+Thommyebere@users.noreply.github.com>
2023-04-05 11:25:37 -0400

    Added ability to delete a todo application

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const date=document.querySelector('#startDate');
 const sub=document.querySelector('.user-input');
 const submitted=document.querySelector('.new-input');
 const gettodos=document.querySelector('.todos');
+const items =document.querySelectorAll('li');
 
 const getelement=()=>{
 sub.addEventListener('submit', e =>{
@@ -37,6 +38,9 @@ const display=(todo, id)=>{
     <div><h3>Status</h3>${todo.Status}</div>
     <div><h3>Execution Date</h3>${todo.DateofActivity}</div>
     <div><h3>Created Date</h3>${time}</div>
+    <button class ="btn btn-danger btn-sm my-2">Delete</button>
+    <button class ="btn btn-secondary btn-sm my-2">update</button>
+
     </li>`;
     gettodos.innerHTML+=html;
 }
@@ -45,7 +49,7 @@ const display=(todo, id)=>{
 
 const fetchdata = ()=>
 {
-    db.collection('todos').get().then((snapshot)=>{
+    db.collection('todos').where('Status', '==','Pending').orderBy('Created_at').get().then((snapshot)=>{
         snapshot.docs.forEach(doc=>{
             display(doc.data(), doc.id);
         })
@@ -53,6 +57,7 @@ const fetchdata = ()=>
             console.log(err);
     });
 }
+
 
 const presentNewDate=()=>{
     console.log("this function was called");
@@ -72,4 +77,37 @@ function pageRedirect(){
     },delay);
  }
 
+ const deletetodo=()=>{
+
+    gettodos.addEventListener('click', e =>{
+        if(e.target.tagName ==='BUTTON')
+        {
+            if(e.target.childNodes[0].data === 'Delete'){
+                console.log("delete got clicked");
+                const id=e.target.parentElement.getAttribute('data-id');
+                db.collection('todos').doc(id).delete();
+                items.forEach(item=>{
+                    if(item.getAttribute('data-id')===id)
+                    {
+                        item.remove();
+                    }
+                })
+            }
+        }
+    });
+ }
+
+//  const unsub= db.collection('todos').where('Status', '==','Pending').orderBy('Created_at').onSnapshotget(snapshot=>{
+//     snapshot.docChanges().forEach(change=>{
+//         const doc=change.doc;
+//         if(change.type==='added')
+//         {
+//             display(doc.data(), doc.id);
+//         }else if(change.type==='removed')
+       
+//     })
+//     }).catch((err)=>{
+//         console.log(err);
+// });
+ deletetodo();
 
